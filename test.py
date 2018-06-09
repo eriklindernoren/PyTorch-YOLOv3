74e993c5f45b2f2883c616f317d53320e7c0aa39
Eugene Shalnov <shalnov.eugen@gmail.com>
2018-06-09 15:10:11 +0300

    Add methods to compute true mAP

--- test.py
+++ test.py
@@ -53,38 +53,50 @@ dataloader = torch.utils.data.DataLoader(
     batch_size=opt.batch_size, shuffle=False, num_workers=opt.n_cpu)
 
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+num_classes = 80
+num_batches = len(dataloader)
 
-n_gt = 0
-correct = 0
+# template to output progress
+num_batch_digits = len(str(num_batches))
+output_template = 'Batch [{0: ' + str(num_batch_digits) + '}/{1}]'
+
+annotations = []
+detections  = []
 for batch_i, (_, imgs, targets) in enumerate(dataloader):
     imgs = Variable(imgs.type(Tensor))
     targets = targets.type(Tensor)
 
+    batch_size = imgs.shape[0]
+
     with torch.no_grad():
         output = model(imgs)
-        output = non_max_suppression(output, 80, conf_thres=0.2)
+        output = non_max_suppression(output, num_classes, conf_thres=0.2)
 
-    for sample_i in range(targets.size(0)):
-        # Get labels for sample where width is not zero (dummies)
+    for sample_i in range(batch_size):
+        # add targets to annotations:
+        # 1. convert from (center, size) to (top-left, bottom-right)
+        # 2. convert from unnormalized coordinates
+        # 3. split into groups corresponding to class labels
         target_sample = targets[sample_i, targets[sample_i, :, 3] != 0]
-        for obj_cls, tx, ty, tw, th in target_sample:
-            # Get rescaled gt coordinates
-            tx1, tx2 = opt.img_size * (tx - tw / 2), opt.img_size * (tx + tw / 2)
-            ty1, ty2 = opt.img_size * (ty - th / 2), opt.img_size * (ty + th / 2)
-            n_gt += 1
-            box_gt = torch.cat([coord.unsqueeze(0) for coord in [tx1, ty1, tx2, ty2]]).view(1, -1)
-            sample_pred = output[sample_i]
-            if sample_pred is not None:
-                # Iterate through predictions where the class predicted is same as gt
-                for x1, y1, x2, y2, conf, obj_conf, obj_pred in sample_pred[sample_pred[:, 6] == obj_cls]:
-                    box_pred = torch.cat([coord.unsqueeze(0) for coord in [x1, y1, x2, y2]]).view(1, -1)
-                    iou = bbox_iou(box_pred, box_gt)
-                    if iou >= opt.iou_thres:
-                        correct += 1
-                        break
+        if target_sample.size(0) == 0: # no annotated objects
+            annotations.append([np.empty((0, 5)) for label in range(num_classes)])
+        else:
+            target_sample[:, 1:3]  = target_sample[:, 1:3] - target_sample[:, 3:5] / 2
+            target_sample[:, 3:5] += target_sample[:, 1:3]
+            target_sample[:, 1:5] *= opt.img_size
+            annotations.append([target_sample[target_sample[:, 0] == label, 1:5]
+                    for label in range(num_classes)])
+        # add output to detections:
+        # 1. split into groups corresponding to class labels
+        sample_pred = output[sample_i]
+        if sample_pred is None: # no detected objects
+            detections.append([np.empty((0, 5)) for label in range(num_classes)])
+        else:
+            detections.append([sample_pred[sample_pred[:, -1] == label, :5]
+                    for label in range(num_classes)])
+    print(output_template.format(batch_i, num_batches), end='\r')
+print()
 
-    if n_gt:
-        print ('Batch [%d/%d] mAP: %.5f' % (batch_i, len(dataloader), float(correct / n_gt)))
-
-
-print ('Mean Average Precision: %.5f' % float(correct / n_gt))
+ap = evaluate(detections, annotations, opt.iou_thres)
+ap_list = [v for k, v in ap.items()]
+print ('Mean Average Precision: {:.5f}'.format(sum(ap_list) / num_classes))
