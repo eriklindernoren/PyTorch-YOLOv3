7dddf2aff16b2ee5c15b42a5c7389fc5d1682b1c
eriklindernoren <eriklindernoren@gmail.com>
2018-06-13 22:21:35 +0200

    Computation of mAP

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ import sys
 import time
 import datetime
 import argparse
+from tqdm import tqdm
 
 import torch
 from torch.utils.data import DataLoader
@@ -21,12 +22,14 @@ import torch.optim as optim
 parser = argparse.ArgumentParser()
 parser.add_argument('--epochs', type=int, default=200, help='number of epochs')
 parser.add_argument('--image_folder', type=str, default='data/samples', help='path to dataset')
-parser.add_argument('--batch_size', type=int, default=1, help='size of each image batch')
+parser.add_argument('--batch_size', type=int, default=16, help='size of each image batch')
 parser.add_argument('--model_config_path', type=str, default='config/yolov3.cfg', help='path to model config file')
 parser.add_argument('--data_config_path', type=str, default='config/coco.data', help='path to data config file')
 parser.add_argument('--weights_path', type=str, default='weights/yolov3.weights', help='path to weights file')
 parser.add_argument('--class_path', type=str, default='data/coco.names', help='path to class label file')
 parser.add_argument('--iou_thres', type=float, default=0.5, help='iou threshold required to qualify as detected')
+parser.add_argument('--conf_thres', type=float, default=0.5, help='object confidence threshold')
+parser.add_argument('--nms_thres', type=float, default=0.45, help='iou thresshold for non-maximum suppression')
 parser.add_argument('--n_cpu', type=int, default=0, help='number of cpu threads to use during batch generation')
 parser.add_argument('--img_size', type=int, default=416, help='size of each image dimension')
 opt = parser.parse_args()
@@ -37,6 +40,7 @@ cuda = True if torch.cuda.is_available else False
 # Get data configuration
 data_config     = parse_data_config(opt.data_config_path)
 test_path       = data_config['valid']
+num_classes     = int(data_config['classes'])
 
 # Initiate model
 model = Darknet(opt.model_config_path)
@@ -48,43 +52,89 @@ if cuda:
 model.eval()
 
 # Get dataloader
-dataloader = torch.utils.data.DataLoader(
-    ListDataset(test_path),
+dataset = ListDataset(test_path)
+dataloader = torch.utils.data.DataLoader(dataset,
     batch_size=opt.batch_size, shuffle=False, num_workers=opt.n_cpu)
 
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
 n_gt = 0
 correct = 0
+
+print ('Compute mAP...')
+
+outputs = []
+targets = None
+APs = []
 for batch_i, (_, imgs, targets) in enumerate(dataloader):
     imgs = Variable(imgs.type(Tensor))
     targets = targets.type(Tensor)
 
     with torch.no_grad():
         output = model(imgs)
-        output = non_max_suppression(output, 80, conf_thres=0.2)
+        output = non_max_suppression(output, 80, conf_thres=opt.conf_thres, nms_thres=opt.nms_thres)
 
+    # Compute average precision for each sample
     for sample_i in range(targets.size(0)):
+        correct = []
+
         # Get labels for sample where width is not zero (dummies)
-        target_sample = targets[sample_i, targets[sample_i, :, 3] != 0]
-        for obj_cls, tx, ty, tw, th in target_sample:
-            # Get rescaled gt coordinates
-            tx1, tx2 = opt.img_size * (tx - tw / 2), opt.img_size * (tx + tw / 2)
-            ty1, ty2 = opt.img_size * (ty - th / 2), opt.img_size * (ty + th / 2)
-            n_gt += 1
-            box_gt = torch.cat([coord.unsqueeze(0) for coord in [tx1, ty1, tx2, ty2]]).view(1, -1)
-            sample_pred = output[sample_i]
-            if sample_pred is not None:
-                # Iterate through predictions where the class predicted is same as gt
-                for x1, y1, x2, y2, conf, obj_conf, obj_pred in sample_pred[sample_pred[:, 6] == obj_cls]:
-                    box_pred = torch.cat([coord.unsqueeze(0) for coord in [x1, y1, x2, y2]]).view(1, -1)
-                    iou = bbox_iou(box_pred, box_gt)
-                    if iou >= opt.iou_thres:
-                        correct += 1
-                        break
+        annotations = targets[sample_i, targets[sample_i, :, 3] != 0]
+        # Extract detections
+        detections = output[sample_i]
 
-    if n_gt:
-        print ('Batch [%d/%d] mAP: %.5f' % (batch_i, len(dataloader), float(correct / n_gt)))
+        if detections is None:
+            # If there are no detections but there are annotations mask as zero AP
+            if annotations.size(0) != 0:
+                APs.append(0)
+            continue
 
+        # Get detections sorted by decreasing confidence scores
+        detections = detections[np.argsort(-detections[:, 4])]
 
-print ('Mean Average Precision: %.5f' % float(correct / n_gt))
+        # If no annotations add number of detections as incorrect
+        if annotations.size(0) == 0:
+            correct.extend([0 for _ in range(len(detections))])
+        else:
+            # Extract target boxes as (x1, y1, x2, y2)
+            target_boxes = torch.FloatTensor(annotations[:, 1:].shape)
+            target_boxes[:, 0] = (annotations[:, 1] - annotations[:, 3] / 2)
+            target_boxes[:, 1] = (annotations[:, 2] - annotations[:, 4] / 2)
+            target_boxes[:, 2] = (annotations[:, 1] + annotations[:, 3] / 2)
+            target_boxes[:, 3] = (annotations[:, 2] + annotations[:, 4] / 2)
+            target_boxes *= opt.img_size
+
+            detected = []
+            for *pred_bbox, conf, obj_conf, obj_pred in detections:
+
+                pred_bbox = torch.FloatTensor(pred_bbox).view(1, -1)
+                # Compute iou with target boxes
+                iou = bbox_iou(pred_bbox, target_boxes)
+                # Extract index of largest overlap
+                best_i = np.argmax(iou)
+                # If overlap exceeds threshold and classification is correct mark as correct
+                if iou[best_i] > opt.iou_thres and obj_pred == annotations[best_i, 0] and best_i not in detected:
+                    correct.append(1)
+                    detected.append(best_i)
+                else:
+                    correct.append(0)
+
+        # Extract true and false positives
+        true_positives  = np.array(correct)
+        false_positives = 1 - true_positives
+
+        # Compute cumulative false positives and true positives
+        false_positives = np.cumsum(false_positives)
+        true_positives  = np.cumsum(true_positives)
+
+        # Compute recall and precision at all ranks
+        recall    = true_positives / annotations.size(0) if annotations.size(0) else true_positives
+        precision = true_positives / np.maximum(true_positives + false_positives, np.finfo(np.float64).eps)
+
+        # Compute average precision
+        AP = compute_ap(recall, precision)
+        APs.append(AP)
+
+        print ("+ Sample [%d/%d] AP: %.4f (%.4f)" % (len(APs), len(dataset), AP, np.mean(APs)))
+
+print("Mean Average Precision: %.4f" % np.mean(APs))
--- utils/utils.py
+++ utils/utils.py
@@ -26,6 +26,33 @@ def weights_init_normal(m):
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0.0)
 
+def compute_ap(recall, precision):
+    """ Compute the average precision, given the recall and precision curves.
+    Code originally from https://github.com/rbgirshick/py-faster-rcnn.
+
+    # Arguments
+        recall:    The recall curve (list).
+        precision: The precision curve (list).
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+    """
+    # correct AP calculation
+    # first append sentinel values at the end
+    mrec = np.concatenate(([0.], recall, [1.]))
+    mpre = np.concatenate(([0.], precision, [0.]))
+
+    # compute the precision envelope
+    for i in range(mpre.size - 1, 0, -1):
+        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+    # to calculate area under PR curve, look for points
+    # where X axis (recall) changes value
+    i = np.where(mrec[1:] != mrec[:-1])[0]
+
+    # and sum (\Delta recall) * prec
+    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap 
+
 def bbox_iou(box1, box2, x1y1x2y2=True):
     """
     Returns the IoU of two bounding boxes
