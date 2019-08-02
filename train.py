ce78d8048e30c39a823306d2a2dc63a5ebea1dac
jason-hu7 <dcjasonhu@gmail.com>
2019-08-02 18:12:55 -0500

    add parameter for ListDataset class in training so it adjust to parse parameters

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
             model.load_darknet_weights(opt.pretrained_weights)
 
     # Get dataloader
-    dataset = ListDataset(train_path, augment=True, multiscale=opt.multiscale_training)
+    dataset = ListDataset(train_path, img_size=opt.img_size, augment=True, multiscale=opt.multiscale_training)
     dataloader = torch.utils.data.DataLoader(
         dataset,
         batch_size=opt.batch_size,
