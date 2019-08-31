90297fb880730fe30a0c6343221a20e5dcb48e38
adriansarno <mr.sarno2@gmail.com>
2019-08-31 13:13:10 -0700

    Fix CUDA with conda environment - Reduce size with tiny-custom.cfg

--- train.py
+++ train.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
+    print('torch version: ', torch.__version__)
     print('-----------------------------------------')
 
     os.makedirs("output", exist_ok=True)
---
+++ train.sh
@@ -0,0 +1,2 @@
+python3 train.py --data_config config/kuzu.data --model_def config/yolov3-tiny-custom.cfg
+
