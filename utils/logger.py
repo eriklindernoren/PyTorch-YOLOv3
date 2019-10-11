d3bdc4416d4c26c98f78b0fa3d42e13ccddb8513
Guillaume <fournierg@gmail.com>
2019-10-12 00:52:34 +0200

    Fix logger for tensorboard with TF2

--- utils/logger.py
+++ utils/logger.py
@@ -11,8 +11,6 @@ class Logger(object):
         with self.writer.as_default():
             tf.summary.scalar(tag, value, step=step)
             self.writer.flush()
-        # summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        # self.writer.add_summary(summary, step)
 
     def list_of_scalars_summary(self, tag_value_pairs, step):
         """Log scalar variables."""
@@ -20,5 +18,3 @@ class Logger(object):
             for tag, value in tag_value_pairs:
                 tf.summary.scalar(tag, value, step=step)
             self.writer.flush()
-        # summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value) for tag, value in tag_value_pairs])
-        # self.writer.add_summary(summary, step)
