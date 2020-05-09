fbeaaab6940169dbf46fc0bf59bd3200565cc932
Koji Minoda <minoda@enaccluster003.epfl.ch>
2020-05-09 03:14:40 +0200

    edit logger.py and generate_ecp_list.py

--- generate_ecp_list.py
+++ generate_ecp_list.py
@@ -11,18 +11,12 @@ import argparse
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--path_ecp", type=str, default="/home/dataset/ECP/ECP/day", help="path to ecp dataset directory. day or night.")
-    parser.add_argument("--path_out", type=str, default="data/ecp", help="output path for .txt files")
-    opt = parser.parse_args()
-    print(opt)
-    
-    ecp_img_train = os.path.join(opt.path_ecp, "img/train")
-    ecp_labels_train = os.path.join(opt.path_ecp, "labels/train")
-    ecp_img_val = os.path.join(opt.path_ecp, "img/val")
-    ecp_labels_val = os.path.join(opt.path_ecp, "labels/val")
-    ecp_img_test = os.path.join(opt.path_ecp, "img/test")
+def get_imgs_labels_lists(path):
+    ecp_img_train = os.path.join(path, "img/train")
+    ecp_labels_train = os.path.join(path, "labels/train")
+    ecp_img_val = os.path.join(path, "img/val")
+    ecp_labels_val = os.path.join(path, "labels/val")
+    ecp_img_test = os.path.join(path, "img/test")
     
     img_train_list = []
     labels_train_list = []
@@ -52,12 +46,35 @@ if __name__ == "__main__":
         folder = os.path.join(ecp_img_test, location)
         for file in os.listdir(folder):
             img_test_list.append(os.path.join(ecp_img_test, location, file))
-            
     img_train_list = natsorted(img_train_list)
     labels_train_list = natsorted(labels_train_list)
     img_val_list = natsorted(img_val_list)
     labels_val_list = natsorted(labels_val_list)
     img_test_list = natsorted(img_test_list)
+    return img_train_list, labels_train_list, img_val_list, labels_val_list, img_test_list
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path_ecp", type=str, default="/home/dataset/ECP/ECP/day", help="path to ecp dataset directory. day or night.")
+    parser.add_argument("--path_out", type=str, default="data/ecp", help="output path for .txt files")
+    opt = parser.parse_args()
+    print(opt)
+    if "day" in os.listdir(opt.path_ecp):
+        print("Use both day and night dataset...")
+        img_train_list, labels_train_list, img_val_list, labels_val_list, img_test_list = get_imgs_labels_lists(os.path.join(opt.path_ecp,"day"))
+        night1, night2, night3, night4, night5 = get_imgs_labels_lists(os.path.join(opt.path_ecp,"night"))
+        img_train_list.extend(night1)
+        labels_train_list.extend(night2)
+        img_val_list.extend(night3)
+        labels_val_list.extend(night4)
+        img_test_list.extend(night5)
+        img_train_list = natsorted(img_train_list)
+        labels_train_list = natsorted(labels_train_list)
+        img_val_list = natsorted(img_val_list)
+        labels_val_list = natsorted(labels_val_list)
+        img_test_list = natsorted(img_test_list)
+    else:
+        img_train_list, labels_train_list, img_val_list, labels_val_list, img_test_list = get_imgs_labels_lists(opt.path_ecp)
 
     path_w = os.path.join(opt.path_out, './train.txt')
     with open(path_w, mode='w') as f:
