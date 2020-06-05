3376ececb358ce465a723bab7bbfb199712b2c45
Junji Hashimoto <junji.hashimoto@gmail.com>
2020-06-05 18:49:05 +0900

    Fix arguments

--- detect.py
+++ detect.py
@@ -32,7 +32,6 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
     parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
-    parser.add_argument("--checkpoint_model", type=str, help="path to checkpoint model")
     opt = parser.parse_args()
     print(opt)
 
