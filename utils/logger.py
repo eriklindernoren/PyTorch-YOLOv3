daad191c7ad2b408f904804c644a30fc3c068072
Florian <florian@flova.de>
2021-01-09 23:53:58 +0100

    But logs in destinct folders This prevents tensorboard from "combining" multiple training runs

--- utils/logger.py
+++ utils/logger.py
@@ -1,10 +1,16 @@
+import os
+import datetime
 from torch.utils.tensorboard import SummaryWriter
 
 
 class Logger(object):
-    def __init__(self, log_dir):
+    def __init__(self, log_dir, log_hist=True):
         """Create a summary writer logging to log_dir."""
-        self.writer = SummaryWriter(log_dir=log_dir)
+        if log_hist:    # Check a new folder for each log should be dreated
+            log_dir = os.path.join(
+                log_dir,
+                datetime.datetime.now().strftime("%Y_%m_%d__%H_%M_%S"))
+        self.writer = SummaryWriter(log_dir)
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
