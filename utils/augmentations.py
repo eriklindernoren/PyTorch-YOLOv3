deb1d5aa5dd2a409918b0baa2eda34c69943267d
Florian <florian@flova.de>
2021-01-10 02:14:20 +0100

    Add augmentation transform

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -1,9 +1,22 @@
-import torch
-import torch.nn.functional as F
-import numpy as np
+import imgaug.augmenters as iaa
+from .transforms import *
+
+class DefaultAug(ImgAug):
+    def __init__(self, ):
+        self.augmentations = iaa.Sequential([
+            iaa.Dropout([0.0, 0.01]),
+            iaa.Sharpen((0.0, 0.2)),
+            iaa.Affine(rotate=(-20, 20), translate_percent=(-0.2,0.2)),  # rotate by -45 to 45 degrees (affects segmaps)
+            iaa.AddToBrightness((-30, 30)), 
+            iaa.AddToHue((-20, 20)),
+            iaa.Fliplr(0.5),
+        ], random_order=True)
 
 
-def horisontal_flip(images, targets):
-    images = torch.flip(images, [-1])
-    targets[:, 2] = 1 - targets[:, 2]
-    return images, targets
+AUGMENTATION_TRANSFORMS = transforms.Compose([
+        AbsoluteLabels(),
+        DefaultAug(),
+        PadSquare(),
+        RelativeLabels(),
+        ToTensor(),
+    ])
