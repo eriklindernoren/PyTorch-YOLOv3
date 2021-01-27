52e9a320a7f4e238f7f92e3ff9f99f5b5e283303
Jan Gutsche <github@jaagut.de>
2021-01-27 16:54:23 +0100

    Move detections to large method

--- detect.py
+++ detect.py
@@ -24,48 +24,60 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.ticker import NullLocator
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Detect objects on images.")
-    parser.add_argument("-m", "--model", type=str, required=True, default="config/yolov3.cfg", help="Path to model definition file (.cfg)")
-    parser.add_argument("-c", "--classes", type=str, default="data/coco.names", help="Path to classes label file (.names)")
-    parser.add_argument("-w", "--weights", type=str, required=True, default="weights/yolov3.weights", help="Path to weights or checkpoint file (.weights or .pth")
-    parser.add_argument("-i", "--images", type=str, default="data/samples", help="Path to directory with images to inference")
-    parser.add_argument("-o", "--output", type=str, default="output", help="Output directory")
-    parser.add_argument("-b", "--batch_size", type=int, default=8, help="Size of each image batch")
-    parser.add_argument("--img_size", type=int, default=416, help="Size of each image dimension for yolo")
-    parser.add_argument("--n_cpu", type=int, default=8, help="Number of cpu threads to use during batch generation")
-    parser.add_argument("--iou_thres", type=float, default=0.5, help="IOU threshold required to qualify as detected")
-    parser.add_argument("--conf_thres", type=float, default=0.5, help="Object confidence threshold")
-    parser.add_argument("--nms_thres", type=float, default=0.5, help="IOU threshold for non-maximum suppression")
-    args = parser.parse_args()
-    print(args)
 
+def detect(model_path, classes_path, weights_path, images_path, output_path,
+    batch_size=8, img_size=416, n_cpu=8, iou_thres=0.5, conf_thres=0.5, nms_thres=0.5):
+    """Inferences images with model and saves detections to output directory.
+
+    :param model_path: Path to model definition file (.cfg)
+    :type model_path: str
+    :param classes_path: Path to classes label file (.names)
+    :type classes_path: str
+    :param weights_path: Path to weights or checkpoint file (.weights or .pth)
+    :type weights_path: str
+    :param images_path: Path to directory with images to inference
+    :type images_path: str
+    :param output_path: Path to output directory
+    :type output_path: str
+    :param batch_size: Size of each image batch, defaults to 8
+    :type batch_size: int, optional
+    :param img_size: Size of each image dimension for yolo, defaults to 416
+    :type img_size: int, optional
+    :param n_cpu: Number of cpu threads to use during batch generation, defaults to 8
+    :type n_cpu: int, optional
+    :param iou_thres: IOU threshold required to qualify as detected, defaults to 0.5
+    :type iou_thres: float, optional
+    :param conf_thres: Object confidence threshold, defaults to 0.5
+    :type conf_thres: float, optional
+    :param nms_thres: IOU threshold for non-maximum suppression, defaults to 0.5
+    :type nms_thres: float, optional
+    """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Create output directories if missing
-    os.makedirs(args.output, exist_ok=True)
+    os.makedirs(output_path, exist_ok=True)
 
     # Set up model
-    model = Darknet(args.model, img_size=args.img_size).to(device)
+    model = Darknet(model_path, img_size=img_size).to(device)
 
-    if args.weights.endswith(".weights"):
+    if weights_path.endswith(".weights"):
         # Load darknet weights
-        model.load_darknet_weights(args.weights)
+        model.load_darknet_weights(weights_path)
     else:
         # Load checkpoint weights
-        model.load_state_dict(torch.load(args.weights))
+        model.load_state_dict(torch.load(weights_path))
 
     model.eval()  # Set in evaluation mode
 
     dataloader = DataLoader(
-        ImageFolder(args.images, transform= \
-            transforms.Compose([DEFAULT_TRANSFORMS, Resize(args.img_size)])),
-        batch_size=args.batch_size,
+        ImageFolder(images_path, transform= \
+            transforms.Compose([DEFAULT_TRANSFORMS, Resize(img_size)])),
+        batch_size=batch_size,
         shuffle=False,
-        num_workers=args.n_cpu,
+        num_workers=n_cpu,
     )
 
-    classes = load_classes(args.classes)  # Extracts class labels from file
+    classes = load_classes(classes_path)  # Extracts class labels from file
 
     Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
@@ -81,7 +93,7 @@ if __name__ == "__main__":
         # Get detections
         with torch.no_grad():
             detections = model(input_imgs)
-            detections = non_max_suppression(detections, args.conf_thres, args.nms_thres)
+            detections = non_max_suppression(detections, conf_thres, nms_thres)
 
         # Log progress
         current_time = time.time()
@@ -112,7 +124,7 @@ if __name__ == "__main__":
         # Draw bounding boxes and labels of detections
         if detections is not None:
             # Rescale boxes to original image
-            detections = rescale_boxes(detections, args.img_size, img.shape[:2])
+            detections = rescale_boxes(detections, img_size, img.shape[:2])
             unique_labels = detections[:, -1].cpu().unique()
             n_cls_preds = len(unique_labels)
             bbox_colors = random.sample(colors, n_cls_preds)
@@ -143,6 +155,36 @@ if __name__ == "__main__":
         plt.gca().xaxis.set_major_locator(NullLocator())
         plt.gca().yaxis.set_major_locator(NullLocator())
         filename = os.path.basename(path).split(".")[0]
-        output_path = os.path.join(args.output, f"{filename}.png")
+        output_path = os.path.join(output_path, f"{filename}.png")
         plt.savefig(output_path, bbox_inches="tight", pad_inches=0.0)
         plt.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Detect objects on images.")
+    parser.add_argument("-m", "--model", type=str, default="config/yolov3.cfg", help="Path to model definition file (.cfg)")
+    parser.add_argument("-c", "--classes", type=str, default="data/coco.names", help="Path to classes label file (.names)")
+    parser.add_argument("-w", "--weights", type=str, default="weights/yolov3.weights", help="Path to weights or checkpoint file (.weights or .pth)")
+    parser.add_argument("-i", "--images", type=str, default="data/samples", help="Path to directory with images to inference")
+    parser.add_argument("-o", "--output", type=str, default="output", help="Path to output directory")
+    parser.add_argument("-b", "--batch_size", type=int, default=8, help="Size of each image batch")
+    parser.add_argument("--img_size", type=int, default=416, help="Size of each image dimension for yolo")
+    parser.add_argument("--n_cpu", type=int, default=8, help="Number of cpu threads to use during batch generation")
+    parser.add_argument("--iou_thres", type=float, default=0.5, help="IOU threshold required to qualify as detected")
+    parser.add_argument("--conf_thres", type=float, default=0.5, help="Object confidence threshold")
+    parser.add_argument("--nms_thres", type=float, default=0.5, help="IOU threshold for non-maximum suppression")
+    args = parser.parse_args()
+    print(args)
+
+    detect(
+        args.model,
+        args.classes,
+        args.weights,
+        args.images,
+        args.output,
+        batch_size=args.batch_size,
+        img_size=args.img_size,
+        n_cpu=args.n_cpu,
+        iou_thres=args.iou_thres,
+        conf_thres=args.conf_thres,
+        nms_thres=args.nms_thres)
