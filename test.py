947bcdd96de4d60770f60e00a4ec4e95f769c347
Jan Gutsche <github@jaagut.de>
2021-01-29 14:37:48 +0100

    Refactor test

--- test.py
+++ test.py
@@ -8,9 +8,6 @@ from utils.transforms import *
 from utils.parse_config import *
 
 import os
-import sys
-import time
-import datetime
 import argparse
 import tqdm
 
@@ -21,49 +18,84 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
 from torch.autograd import Variable
-import torch.optim as optim
 
-def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size):
+
+def evaluate(model_path, weights_path, img_path,
+    batch_size=8, img_size=416, n_cpu=8,
+    iou_thres=0.5, conf_thres=0.5, nms_thres=0.5, print_stats=True):
+    """Evaluate model on validation dataset.
+
+    :param model_path: Path to model definition file (.cfg)
+    :type model_path: str
+    :param weights_path: Path to weights or checkpoint file (.weights or .pth)
+    :type weights_path: str
+    :param img_path: Path to file containing all paths to validation images.
+    :type img_path: str
+    :param batch_size: Size of each image batch, defaults to 8
+    :type batch_size: int, optional
+    :param img_size: Size of each image dimension for yolo, defaults to 416
+    :type img_size: int, optional
+    :param n_cpu: Number of cpu threads to use during batch generation, defaults to 8
+    :type n_cpu: int, optional
+    :param iou_thres: IOU threshold required to qualify as detected, defaults to 0.5
+    :type iou_thres: float, optional
+    :param conf_thres: Object confidence threshold, defaults to 0.5
+    :type conf_thres: float, optional
+    :param nms_thres: IOU threshold for non-maximum suppression, defaults to 0.5
+    :type nms_thres: float, optional
+    :param print_stats: If True, prints stats of model, defaults to True
+    :type print_stats: bool, optional
+    :return: Returns precision, recall, AP, f1, ap_class
+    """
+    dataloader = _create_validation_data_loader(img_path, batch_size, img_size, n_cpu)
+    model = _load_model(model_path, weights_path)
+    precision, recall, AP, f1, ap_class = _evaluate(
+        model,
+        dataloader,
+        img_size,
+        iou_thres,
+        conf_thres,
+        nms_thres)
+    if print_stats:
+        print_eval_stats(AP, ap_class, class_names)
+    return precision, recall, AP, f1, ap_class
+
+def print_eval_stats(AP, ap_class, class_names):
+    # Prints class AP and mean AP
+    ap_table = [["Index", "Class", "AP"]]
+    for i, c in enumerate(ap_class):
+        ap_table += [[c, class_names[c], "%.5f" % AP[i]]]
+    print(AsciiTable(ap_table).table)
+    print(f"---- mAP {AP.mean():.5f} ----")
+
+def _evaluate(model, dataloader, img_size, iou_thres, conf_thres, nms_thres):
     """Evaluate model on validation dataset.
 
     :param model: Model to evaluate
-    :type model: [type]
-    :param path: Path to file listing all validation image-paths
-    :type path: str
+    :type model: models.Darknet
+    :param dataloader: Dataloader provides the batches of images with targets
+    :type dataloader: DataLoader
+    :param img_size: Size of each image dimension for yolo
+    :type img_size: int
     :param iou_thres: IOU threshold required to qualify as detected
     :type iou_thres: float
     :param conf_thres: Object confidence threshold
     :type conf_thres: float
     :param nms_thres: IOU threshold for non-maximum suppression
     :type nms_thres: float
-    :param img_size: Size of each image dimension for yolo
-    :type img_size: int
-    :param batch_size: Size of each image batch
-    :type batch_size: int
-    :return: precision, recall, AP, f1, ap_class
-    :rtype: [np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]
+    :return: Returns precision, recall, AP, f1, ap_class
     """
     model.eval()  # Set model to evaluation mode
-
-    # Get dataloader
-    dataset = ListDataset(path, img_size=img_size, multiscale=False, transform=DEFAULT_TRANSFORMS)
-    dataloader = torch.utils.data.DataLoader(
-        dataset, 
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=1,
-        collate_fn=dataset.collate_fn
-    )
 
     Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
     labels = []
     sample_metrics = []  # List of tuples (TP, confs, pred)
-    for batch_i, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc="Detecting objects")):
-        
+    for (_, imgs, targets) in tqdm.tqdm(dataloader, desc="Validating"):
+
         if targets is None:
             continue
-            
+
         # Extract labels
         labels += targets[:, 1].tolist()
         # Rescale target
@@ -84,20 +116,53 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
 
     return precision, recall, AP, f1, ap_class
 
-    def print_eval_stats(AP, ap_class, class_names):
-        # Prints class AP and mean AP
-        ap_table = [["Index", "Class", "AP"]]
-        for i, c in enumerate(ap_class):
-            ap_table += [[c, class_names[c], "%.5f" % AP[i]]]
-        print(AsciiTable(ap_table).table)
-        print(f"---- mAP {AP.mean():.5f} ----")
+def _create_validation_data_loader(img_path, batch_size, img_size, n_cpu):
+    """Creates a DataLoader for validation.
+
+    :param img_path: Path to file containing all paths to validation images.
+    :type img_path: str
+    :param batch_size: Size of each image batch
+    :type batch_size: int
+    :param img_size: Size of each image dimension for yolo
+    :type img_size: int
+    :param n_cpu: Number of cpu threads to use during batch generation
+    :type n_cpu: int
+    :return: Returns DataLoader
+    :rtype: DataLoader
+    """
+    dataset = ListDataset(img_path, image_size=img_size, multiscale=False, transform=DEFAULT_TRANSFORMS)
+    return dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=n_cpu,
+        collate_fn=dataset.collate_fn
+    )
+
+def _load_model(model_path, weights_path):
+    """Loads the yolo model from file.
+
+    :param model_path: Path to model definition file (.cfg)
+    :type model_path: str
+    :param weights_path: Path to weights or checkpoint file (.weights or .pth)
+    :type weights_path: str
+    :return: Returns model
+    :rtype: Darknet
+    """
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Select device for inference
+    model = Darknet(model_path).to(device)
+    if weights_path.endswith(".weights"):  # Load darknet weights
+        model.load_darknet_weights(weights_path)
+    else:  # Load checkpoint weights
+        model.load_state_dict(torch.load(weights_path))
+    return model
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate validation data.")
     parser.add_argument("-m", "--model", type=str, default="config/yolov3.cfg", help="Path to model definition file (.cfg)")
-    parser.add_argument("-d", "--data", type=str, default="config/coco.data", help="Path to data config file (.data)")
     parser.add_argument("-w", "--weights", type=str, default="weights/yolov3.weights", help="Path to weights or checkpoint file (.weights or .pth)")
+    parser.add_argument("-d", "--data", type=str, default="config/coco.data", help="Path to data config file (.data)")
     parser.add_argument("-b", "--batch_size", type=int, default=8, help="Size of each image batch")
     parser.add_argument("--img_size", type=int, default=416, help="Size of each image dimension for yolo")
     parser.add_argument("--n_cpu", type=int, default=8, help="Number of cpu threads to use during batch generation")
@@ -107,29 +172,19 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(args)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+    # Load configuration from data file
     data_config = parse_data_config(args.data)
-    valid_path = data_config["valid"]
-    class_names = load_classes(data_config["names"])
-
-    # Initiate model
-    model = Darknet(args.model).to(device)
-    if args.weights.endswith(".weights"):
-        # Load darknet weights
-        model.load_darknet_weights(args.weights)
-    else:
-        # Load checkpoint weights
-        model.load_state_dict(torch.load(args.weights))
+    valid_path = data_config["valid"]  # Path to file containing all images for validation
+    class_names = load_classes(data_config["names"])  # List of class names
 
     precision, recall, AP, f1, ap_class = evaluate(
-        model,
-        path=valid_path,
+        args.model,
+        args.weights
+        valid_path,
+        batch_size=args.batch_size,
+        img_size=args.img_size,
+        n_cpu=args.n_cpu
         iou_thres=args.iou_thres,
         conf_thres=args.conf_thres,
         nms_thres=args.nms_thres,
-        img_size=args.img_size,
-        batch_size=args.batch_size,
-    )
-
-    print_eval_stats(AP, ap_class, class_names)
+        print_stats=True)
