b8a231aa72bff12d94cdc8baf8cc40e7c8bef3f4
imwxc <49653609+imwxc@users.noreply.github.com>
2021-04-01 18:45:06 +0800

    for issue#339
    maybe an augmentation cause the target tensor become empty( tensor([ ]) ) , my solution is comment the Affine out so that the bug will be fixed

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -20,3 +20,22 @@ AUGMENTATION_TRANSFORMS = transforms.Compose([
         RelativeLabels(),
         ToTensor(),
     ])
+
+class Fix_iousNone_Aug(ImgAug):
+    def __init__(self, ):
+        self.augmentations = iaa.Sequential([
+            iaa.Dropout([0.0, 0.01]),
+            iaa.Sharpen((0.0, 0.1)),
+            #iaa.Affine(rotate=(-10, 10), translate_percent=(-0.1,0.1)),  # rotate by -45 to 45 degrees (affects segmaps)
+            iaa.AddToBrightness((-10, 10)), 
+            iaa.AddToHue((-5, 5)),
+            iaa.Fliplr(0.5),
+        ])
+
+AUGMENTATION_TRANSFORMS_fix_iousNone = transforms.Compose([
+        AbsoluteLabels(),
+        Fix_iousNone_Aug(),
+        PadSquare(),
+        RelativeLabels(),
+        ToTensor(),
+    ])
