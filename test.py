f451cf79687130b994cf8682056520d2b25386a5
Florian <florian@flova.de>
2021-04-05 15:24:28 +0200

    Remove unused check

--- test.py
+++ test.py
@@ -38,11 +38,7 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
 
     labels = []
     sample_metrics = []  # List of tuples (TP, confs, pred)
-    for batch_i, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc="Detecting objects")):
-        
-        if targets is None:
-            continue
-            
+    for batch_i, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc="Detecting objects")):          
         # Extract labels
         labels += targets[:, 1].tolist()
         # Rescale target
