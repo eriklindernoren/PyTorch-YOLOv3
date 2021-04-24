671030eb86037c4d12ddd8519f767f1ee3927db5
Florian Vahl <florian@flova.de>
2021-04-25 01:36:50 +0200

    Fix error related to cpu only machines

--- pytorchyolo/models.py
+++ pytorchyolo/models.py
@@ -292,7 +292,7 @@ def load_model(model_path, weights_path=None):
     if weights_path:
         if weights_path.endswith(".pth"):
             # Load checkpoint weights
-            model.load_state_dict(torch.load(weights_path))
+            model.load_state_dict(torch.load(weights_path, map_location=device))
         else:
             # Load darknet weights
             model.load_darknet_weights(weights_path)
