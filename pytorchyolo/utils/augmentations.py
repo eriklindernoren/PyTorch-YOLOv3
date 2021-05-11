1110b5c7680b00aec38076b702a5bef201a6262f
Florian <florian@flova.de>
2021-05-11 17:40:01 +0200

    Tweak augmentations

--- pytorchyolo/utils/augmentations.py
+++ pytorchyolo/utils/augmentations.py
@@ -6,12 +6,22 @@ from pytorchyolo.utils.transforms import ToTensor, PadSquare, RelativeLabels, Ab
 class DefaultAug(ImgAug):
     def __init__(self, ):
         self.augmentations = iaa.Sequential([
+            iaa.Sharpen((0.0, 0.1)),
+            iaa.Affine(rotate=(-0, 0), translate_percent=(-0.1, 0.1), scale=(0.8, 1.5)),
+            iaa.AddToBrightness((-60, 40)),
+            iaa.AddToHue((-10, 10)),
+            iaa.Fliplr(0.5),
+        ])
+
+
+class StrongAug(ImgAug):
+    def __init__(self, ):
+        self.augmentations = iaa.Sequential([
             iaa.Dropout([0.0, 0.01]),
             iaa.Sharpen((0.0, 0.1)),
-            # rotate by -45 to 45 degrees (affects segmaps)
-            iaa.Affine(rotate=(-10, 10), translate_percent=(-0.1, 0.1)),
-            iaa.AddToBrightness((-10, 10)),
-            iaa.AddToHue((-5, 5)),
+            iaa.Affine(rotate=(-10, 10), translate_percent=(-0.1, 0.1), scale=(0.8, 1.5)),
+            iaa.AddToBrightness((-60, 40)),
+            iaa.AddToHue((-20, 20)),
             iaa.Fliplr(0.5),
         ])
 
