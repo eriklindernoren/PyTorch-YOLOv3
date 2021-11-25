38343366cc2825f13cab04d1402b3d87f7df9eff
Florian Vahl <florian@flova.de>
2021-11-25 16:28:29 +0100

    Fix grid index to prvent deprication warning

--- pytorchyolo/models.py
+++ pytorchyolo/models.py
@@ -160,7 +160,7 @@ class YOLOLayer(nn.Module):
 
     @staticmethod
     def _make_grid(nx=20, ny=20):
-        yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
+        yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)], indexing='ij')
         return torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
 
 
